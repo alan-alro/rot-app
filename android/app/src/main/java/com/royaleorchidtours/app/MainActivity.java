6dc55d3ffb26e9174128c41e2ef22b94de482be4
Alan Chen <alan@alromedia.com>
2021-11-23 00:42:31 -0800

    rename apps

--- android/app/src/main/java/com/royaleorchidtours/app/MainActivity.java
+++ android/app/src/main/java/com/royaleorchidtours/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.royaleorchidtours;
+package com.royaleorchidtours.app;
 import expo.modules.ReactActivityDelegateWrapper;
 import com.facebook.react.ReactActivityDelegate;
 
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RoyaleOrchidTours";
+    return "RoyaleorchidTours";
   }
 
   @Override
--- ios/RoyaleOrchidToursTests/RoyaleOrchidToursTests.m
+++ ios/RoyaleOrchidToursTests/RoyaleOrchidToursTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RoyaleOrchidToursTests : XCTestCase
+@interface RoyaleorchidToursTests : XCTestCase
 
 @end
 
-@implementation RoyaleOrchidToursTests
+@implementation RoyaleorchidToursTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
