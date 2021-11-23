1bcee62f206f6007b98a27e41898df181132cb1d
Alan Chen <alan@alromedia.com>
2021-11-23 01:09:28 -0800

    Revert "rename apps"
    This reverts commit 6dc55d3ffb26e9174128c41e2ef22b94de482be4.

--- android/app/src/main/java/com/royaleorchidtours/MainApplication.java
+++ android/app/src/main/java/com/royaleorchidtours/MainApplication.java
@@ -1,4 +1,4 @@
-package com.royaleorchidtours.app;
+package com.royaleorchidtours;
 import android.content.res.Configuration;
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
@@ -66,7 +66,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.royaleorchidtours.app.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.royaleorchidtours.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
