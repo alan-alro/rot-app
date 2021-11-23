655a2e3600d0f9518f04dc3ed7ea5e31b67ec379
Alan Chen <alan@alromedia.com>
2021-11-23 13:14:09 -0800

    codepush

--- ios/RoyaleOrchidTours/AppDelegate.m
+++ ios/RoyaleOrchidTours/AppDelegate.m
@@ -9,6 +9,8 @@
 #import <AppCenterReactNativeCrashes.h>
 
 #import "RNBootSplash.h"
+#import <CodePush/CodePush.h>
+
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -69,7 +71,8 @@ static void InitializeFlipper(UIApplication *application) {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
+  // return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
